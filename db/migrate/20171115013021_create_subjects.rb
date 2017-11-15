232852556ba843991868ede4ef78344295d077e6
Oswaldo <oswaldoalvaradolopez94@gmail.com>
2017-11-15 10:49:14 -0400

    view news

--- db/migrate/20171115013021_create_subjects.rb
+++ db/migrate/20171115013021_create_subjects.rb
@@ -1,15 +1,16 @@
 class CreateSubjects < ActiveRecord::Migration[5.1]
 def self.up
-      
+
       create_table :subjects do |t|
          t.column :name, :string
       end
-	
+
       Subject.create :name => "Physics"
       Subject.create :name => "Mathematics"
       Subject.create :name => "Chemistry"
       Subject.create :name => "Psychology"
       Subject.create :name => "Geography"
+      Subject.create :name => "Telecomunication"
    end
 
    def self.down
